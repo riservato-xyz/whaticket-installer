aa4403ea05ccea8c468afb10d8fedc06ce4c540a
Thiago Melo <dev@riservato.xyz>
2021-10-04 17:25:10 -0300

    feat: frontend_nginx_setup

--- lib/_frontend.sh
+++ lib/_frontend.sh
@@ -85,25 +85,29 @@ EOF
 #######################################
 frontend_nginx_setup() {
 
-  sudo su - deploy <<EOF
-  sudo touch /etc/nginx/sites-available/whaticket-frontend
+  frontend_url=https://myapp.mydomain.com
+  frontend_url=$(echo "${frontend_url/https:\/\/}")
 
-  server {
-    server_name myapp.mydomain.com;
+sudo su - root << EOF
 
-    location / {
-      proxy_pass http://127.0.0.1:3333;
-      proxy_http_version 1.1;
-      proxy_set_header Upgrade $http_upgrade;
-      proxy_set_header Connection 'upgrade';
-      proxy_set_header Host $host;
-      proxy_set_header X-Real-IP $remote_addr;
-      proxy_set_header X-Forwarded-Proto $scheme;
-      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-      proxy_cache_bypass $http_upgrade;
-    }
+cat > /etc/nginx/sites-available/whaticket-frontend << 'END'
+server {
+  server_name $frontend_url;
+
+  location / {
+    proxy_pass http://127.0.0.1:3333;
+    proxy_http_version 1.1;
+    proxy_set_header Upgrade \$http_upgrade;
+    proxy_set_header Connection 'upgrade';
+    proxy_set_header Host \$host;
+    proxy_set_header X-Real-IP \$remote_addr;
+    proxy_set_header X-Forwarded-Proto \$scheme;
+    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    proxy_cache_bypass \$http_upgrade;
   }
+}
+END
 
-  sudo ln -s /etc/nginx/sites-available/whaticket-frontend /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/whaticket-frontend /etc/nginx/sites-enabled
 EOF
 }
