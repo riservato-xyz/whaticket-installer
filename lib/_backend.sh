d26430f9f95bd22802931da3982a5fdd898095f1
Thiago Melo <dev@riservato.xyz>
2021-10-04 20:37:25 -0300

    feat: function order

--- lib/_backend.sh
+++ lib/_backend.sh
@@ -67,66 +67,6 @@ EOF
 }
 
 #######################################
-# Ask for file location containing
-# multiple URL for streaming.
-# Globals:
-#   WHITE
-#   GRAY_LIGHT
-#   BATCH_DIR
-#   PROJECT_ROOT
-# Arguments:
-#   None
-#######################################
-backend_puppeteer_dependencies() {
-
-  sudo su - deploy <<EOF
-  sudo apt-get install -y libxshmfence-dev \
-                          libgbm-dev \
-                          wget \
-                          unzip \
-                          fontconfig \
-                          locales \
-                          gconf-service \
-                          libasound2 \
-                          libatk1.0-0 \
-                          libc6 \
-                          libcairo2 \
-                          libcups2 \
-                          libdbus-1-3 \
-                          libexpat1 \
-                          libfontconfig1 \
-                          libgcc1 \
-                          libgconf-2-4 \
-                          libgdk-pixbuf2.0-0 \
-                          libglib2.0-0 \
-                          libgtk-3-0 \
-                          libnspr4 \
-                          libpango-1.0-0 \
-                          libpangocairo-1.0-0 \
-                          libstdc++6 \
-                          libx11-6 \
-                          libx11-xcb1 \
-                          libxcb1 \
-                          libxcomposite1 \
-                          libxcursor1 \
-                          libxdamage1 \
-                          libxext6 \
-                          libxfixes3 \
-                          libxi6 \
-                          libxrandr2 \
-                          libxrender1 \
-                          libxss1 \
-                          libxtst6 \
-                          ca-certificates \
-                          fonts-liberation \
-                          libappindicator1 \
-                          libnss3 \
-                          lsb-release \
-                          xdg-utils
-EOF
-}
-
-#######################################
 # compiles backend code
 # Arguments:
 #   None
@@ -249,10 +189,6 @@ backend_certbot_setup() {
 
   frontend_url=https://myapp.mydomain.com
   frontend_url=$(echo "${frontend_url/https:\/\/}")
-
-  echo $deploy_email
-  echo $backend_url
-  echo $frontend_url
 
   sudo su - root <<EOF
   certbot -m $deploy_email \
--- lib/_system.sh
+++ lib/_system.sh
@@ -80,6 +80,33 @@ EOF
 }
 
 #######################################
+# Ask for file location containing
+# multiple URL for streaming.
+# Globals:
+#   WHITE
+#   GRAY_LIGHT
+#   BATCH_DIR
+#   PROJECT_ROOT
+# Arguments:
+#   None
+#######################################
+system_puppeteer_dependencies() {
+
+  sudo su - root <<EOF
+  apt-get install -y libxshmfence-dev libgbm-dev wget unzip fontconfig \
+                      locales gconf-service libasound2 .0-0 libc6 libcairo2 \
+                      libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 \
+                      libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 \
+                      libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 \
+                      libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 \
+                      libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 \
+                      libxrender1 libxss1 libxtst6 ca-certificates \
+                      fonts-liberation libappindicator1 libnss3 lsb-release \
+                      xdg-utils
+EOF
+}
+
+#######################################
 # installs pm2
 # Arguments:
 #   None
@@ -114,8 +141,6 @@ EOF
 #   None
 #######################################
 system_certbot_install() {
-
-  deploy_email=deploy@whaticket.com
 
   sudo su - root <<EOF
   apt-get remove certbot
