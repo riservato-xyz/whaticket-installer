a76c42d3e500648ed9c6987b05d7495d92292ce3
igorzeni <127792314+igorzeni@users.noreply.github.com>
2023-03-13 17:50:46 -0300

    Update _system.sh

--- lib/_system.sh
+++ lib/_system.sh
@@ -35,7 +35,7 @@ system_git_clone() {
   sleep 2
 
   sudo su - deploy <<EOF
-  git clone https://github.com/canove/whaticket /home/deploy/whaticket/
+  git clone https://github.com/dollyzn/whaticket-cero /home/deploy/whaticket/
 EOF
 
   sleep 2
